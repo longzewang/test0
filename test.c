0be34e53d8a033ca5a274c0229bd99873124b222
wanglongze <longze@ip-172-31-10-232.ec2.internal>
2017-07-06 04:02:40 +0000

    t

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+int  main()
+{
+int cnt  = 0;
+
+
+    if(cnt==0) {
+        printf("SUCCESS\n");
+        char file_name_s[256] = "pass.o";
+        FILE *fp_s=fopen(file_name_s,"ab+");
+        if(fp_s==NULL) printf("can't create file!");
+    }
+    else {
+        printf("FAIL\n");
+        printf("%d\n", cnt);
+        char file_name_f[256] = "fail.o";
+        FILE *fp_f=fopen(file_name_f,"ab+");
+        if(fp_f==NULL) printf("can't create file!");
+    }
+
+return cnt;
+
+}
